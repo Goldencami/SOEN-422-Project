474e37de2ea9b3ab3159d01ac10a1245f1e7fc99
Camila Guzman <camila.guzman023@gmail.com>
2025-11-18 23:48:44 -0500

    setup servo
    Co-Authored-By: Keshan Kathiripilay <102919093+katkes@users.noreply.github.com>

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -1,3 +1,4 @@
+#include <ESP32Servo.h>
 #define TX_PIN 4
 #define RX_PIN 13
 #define SERVO_PIN 19
@@ -5,7 +6,37 @@
 #define GREEN_LED_PIN 12
 #define RED_LED_PIN 22
 
-String code = "test1234"
+String correctCode = "test1234";
+
+unsigned long servoDelay = 20; // change servo position every 20ms
+unsigned long servoLastDelay = 0;
+
+Servo myServo; // create servo object to control a servo
+int pos = 0; // variable to store the servo position
+bool isUnlocking = false;
+bool isLocking = false;
+
+// servo will rotate from 0 to 180 degrees
+void unlockDoor() {
+  unsigned long timeNow = millis();
+  if ((timeNow - servoLastDelay > servoDelay) && pos < 180) {
+    myServo.write(pos);
+    Serial.println("Unlocking: pos = " + String(pos));
+    ++pos;
+    servoLastDelay = timeNow;
+  }
+}
+
+// servo will rotate from 180 to 0 degrees
+void lockDoor() {
+  unsigned long timeNow = millis();
+  if ((timeNow - servoLastDelay > servoDelay) && pos > 0) {
+    myServo.write(pos);
+    Serial.println("Locking: pos = " + String(pos));
+    --pos;
+    servoLastDelay = timeNow;
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -17,19 +48,22 @@ void setup() {
 
 void loop() {
   if (Serial2.available()) { // Only read if data is present
-    String msg = Serial2.readString(); // Read full message
-    msg.trim(); // remove any trailing newline or spaces
+    String data = Serial2.readString(); // Read full message
+    data.trim(); // remove any trailing newline or spaces
 
     Serial.print("Message Received: ");
-    Serial.println(msg);
+    Serial.println(data);
 
-    if (msg == "1") {
+    if(correctCode == data) {
       digitalWrite(GREEN_LED_PIN, HIGH);
       digitalWrite(RED_LED_PIN, LOW);
     }
-    else if (msg == "0") {
+    else {
       digitalWrite(GREEN_LED_PIN, LOW);
       digitalWrite(RED_LED_PIN, HIGH);
     }
+    delay(500);
+    digitalWrite(GREEN_LED_PIN, LOW);
+    digitalWrite(RED_LED_PIN, LOW);
   }
 }
