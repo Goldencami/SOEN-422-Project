726a21a03896ad55a95161b762e6ce0ca18d4a63
Camila Guzman <camila.guzman023@gmail.com>
2025-11-22 21:12:16 -0500

    setup correct library version for card reader
    Co-Authored-By: Keshan Kathiripilay <102919093+katkes@users.noreply.github.com>

--- DumpInfo/DumpInfo.ino
+++ DumpInfo/DumpInfo.ino
@@ -1,14 +1,14 @@
 /*
  * --------------------------------------------------------------------------------------------------------------------
- * Example sketch/program showing how to read data from a PICC to serial.
+ * Example showing how to read data from a PICC to serial.
  * --------------------------------------------------------------------------------------------------------------------
- * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
+ * This is a MFRC522 library example; for further details and other examples see: https://github.com/OSSLibraries/Arduino_MFRC522v2
  * 
  * Example sketch/program showing how to read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
  * Reader on the Arduino SPI interface.
  * 
  * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
- * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
+ * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shift+M). When
  * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
  * will show the ID/UID, type and any data blocks it can read. Note: you may see "Timeout in communication" messages
  * when removing the PICC from reading distance too early.
@@ -26,44 +26,45 @@
  *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
  * Signal      Pin          Pin           Pin       Pin        Pin              Pin
  * -----------------------------------------------------------------------------------------
- * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
  * SPI SS      SDA(SS)      10            53        D10        10               10
  * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
  * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
  * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
  *
- * More pin layouts for other boards can be found here: https://github.com/miguelbalboa/rfid#pin-layout
+ * Not found? For more see: https://github.com/OSSLibraries/Arduino_MFRC522v2#pin-layout
  */
 
-#include <SPI.h>
-#include <MFRC522.h>
+#include <MFRC522v2.h>
+#include <MFRC522DriverSPI.h>
+//#include <MFRC522DriverI2C.h>
+#include <MFRC522DriverPinSimple.h>
+#include <MFRC522Debug.h>
 
-#define RST_PIN         9          // Configurable, see typical pin layout above
-#define SS_PIN          10         // Configurable, see typical pin layout above
+MFRC522DriverPinSimple ss_pin(10); // Configurable, see typical pin layout above.
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+MFRC522DriverSPI driver{ss_pin}; // Create SPI driver.
+//MFRC522DriverI2C driver{}; // Create I2C driver.
+MFRC522 mfrc522{driver};  // Create MFRC522 instance.
 
 void setup() {
-	Serial.begin(9600);		// Initialize serial communications with the PC
-	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-	SPI.begin();			// Init SPI bus
-	mfrc522.PCD_Init();		// Init MFRC522
-	delay(4);				// Optional delay. Some board do need more time after init to be ready, see Readme
-	mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
+  Serial.begin(115200);  // Initialize serial communications with the PC for debugging.
+  while (!Serial);     // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4).
+  mfrc522.PCD_Init();  // Init MFRC522 board.
+  MFRC522Debug::PCD_DumpVersionToSerial(mfrc522, Serial);	// Show details of PCD - MFRC522 Card Reader details.
 	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 }
 
 void loop() {
 	// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-	if ( ! mfrc522.PICC_IsNewCardPresent()) {
+	if ( !mfrc522.PICC_IsNewCardPresent()) {
 		return;
 	}
 
-	// Select one of the cards
-	if ( ! mfrc522.PICC_ReadCardSerial()) {
+	// Select one of the cards.
+	if ( !mfrc522.PICC_ReadCardSerial()) {
 		return;
 	}
 
-	// Dump debug info about the card; PICC_HaltA() is automatically called
-	mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+	// Dump debug info about the card; PICC_HaltA() is automatically called.
+  MFRC522Debug::PICC_DumpToSerial(mfrc522, Serial, &(mfrc522.uid));
 }
